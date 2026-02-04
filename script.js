e62e091b2f5421c84de1abd53e12ea3402c1a0cf
danieldiugy <danieldiugy@outlook.pt>
2026-02-04 02:58:29 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.querySelectorAll('.dots').forEach(b=>b.addEventListener('click',e=>{e.preventDefault();e.stopPropagation();}));
+// ano no footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// impede que clicar nos ⋮ abra o link
+document.querySelectorAll(".dots").forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+});

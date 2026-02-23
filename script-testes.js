1d1f965ef2deddc7ef02d37f0824f20f82c06d9f
danieldiugy <danieldiugy@outlook.pt>
2026-02-23 19:14:52 +0000

    Update script-testes.js

--- script-testes.js
+++ script-testes.js
@@ -58,7 +58,7 @@ function atualizarSeguidores() {
     adicionarSeguidores('tiktok2-followers', tiktok2Followers);
 
     // TikTok @livesdojust
-    const tiktok3Followers = 15000; // 15K → "15.000+"
+    const tiktok3Followers = 500; // 500 → "500+"
     adicionarSeguidores('tiktok3-followers', tiktok3Followers);
 
     // YouTube @just99500
